2e3776e1360c4da51e6182e83dce45c20cdfeaa3
bees1114 <bees1114@naver.com>
2017-10-31 14:39:18 +0900

    init commit

---
+++ Calender/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.views.generic import View
+import datetime
+import calendar
+
+class FullCalenderView(View):
+    def get(self, request, *args, **kwargs):
+        today_datetime = datetime.datetime.today()
+        this_year = today_datetime.year
+        this_month = today_datetime.month
+        start_day = calendar.monthrange(this_year, this_month)[0]
+        end_date = calendar.monthrange(this_year, this_month)[1]
+        return render(request, 'calender.html',
+                      {'this_year': this_year, 'this_month': this_month, 'start_day': start_day, 'end_date': end_date})
